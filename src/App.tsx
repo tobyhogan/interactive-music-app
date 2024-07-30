be2866987078bc7c31df05f387e905c807f2cd55
Toby Hogan <53061712+tobyhogan@users.noreply.github.com>
2024-07-30 22:50:18 +0100

    .

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,29 @@ function App() {
 
   return (
     <>
-      <h1 className='bg-blue-500 text-5xl mx-auto mt-5 px-10 py-4 w-fit'>React. Vite. Tailwind.</h1>
+      <h1 className='text-3xl mx-auto mt-5 px-10 py-4 w-fit'>Music App</h1>
+
+      <h2>Scale:</h2>
+      <h2>Instrument:</h2>
+
+      <div className='flex flex-row w-fit mx-auto mt-16'>
+
+        <button className='MusicButton1'></button>
+        <button className='MusicButton1'></button>
+        <button className='MusicButton1'></button>
+        <button className='MusicButton1'></button>
+        <button className='MusicButton1'></button>
+        <button className='MusicButton1'></button>
+        <button className='MusicButton1'></button>
+        <button className='MusicButton1'></button>
+        <button className='MusicButton1'></button>
+        <button className='MusicButton1'></button>
+        <button className='MusicButton1'></button>
+        <button className='MusicButton1'></button>
+
+
+
+      </div>
     </>
   )
 }
